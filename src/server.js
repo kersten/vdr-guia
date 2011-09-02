58557a6c222c735932c886ebf4d5bc2a7e70f80c
Kersten Burkhardt <kerstenk@gmail.com>
2011-09-02 16:06:45 +0200

    Made timeline more stable

--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var app = express.createServer();
 var rest = require('restler');
-var microtime = require('microtime');
 
 app.configure('development', function(){
     app.use(express.static(__dirname + '/../public'));
@@ -18,16 +17,40 @@ app.use(express.bodyParser());
 app.use(express.cookieParser());
 app.use(express.session({secret: "keyboard cat", store: new RedisStore}));
 
-app.get('/', function(req, res) {
+app.all('*', function (req, res, next) {
+    if (req.url == '/login') {
+        next();
+        return;
+    }
+    
     if (typeof(req.session.loggedIn) == 'undefined' || !req.session.loggedIn) {
         req.session.loggedIn = false;
         res.redirect('/login');
         return;
     }
+
+    next();
+});
+
+app.get('/timeline', function(req, res) {
+    var channels = new Object();
     
-    var channels = new Array();
-    
-    rest.get('http://192.168.0.192:8002/channels/.json?&start=0&limit=10').on('complete', function(data) {
+    rest.get('http://127.0.0.1:8002/channels/.json?&start=0&limit=10').on('complete', function(data, entry) {
+        var render = function () {
+            if (data.channels.length != waitForFinish) return;
+
+            channels = ksort(channels);
+
+            res.render('timeline', {
+                global: {
+                    title: 'Timeline',
+                    loggedIn: req.session.loggedIn,
+                    page: 'timeline'
+                },
+                channels: channels
+            });
+        };
+        
         /*
          * name: 'arte HD',
          * number: 5,
@@ -43,32 +66,25 @@ app.get('/', function(req, res) {
          * is_radio: false
          */
         
-        var waitForFinish = 0;
+        var waitForFinish = 1;
         
         data.channels.forEach(function (channel) {
-            rest.get('http://192.168.0.192:8002/events/' + channel.channel_id + '/86400.json ').on('complete',  function (data) {
-                channel.epg = data;
-                channels.push(channel);
+            rest.get('http://127.0.0.1:8002/events/' + channel.channel_id + '/86000.json?start=0', {channel: channel}).on('complete',  function (epg) {
+                channels[this.options.channel.number] = {
+                    channel: this.options.channel,
+                    epg: epg
+                };
                 
+                waitForFinish++;
+                render();
+            }).on('error', function (err) {
+                waitForFinish++;
+                render();
+            }).on('404', function () {
                 waitForFinish++;
                 render();
             });
         });
-        
-        var render = function () {
-            if (data.channels.length != waitForFinish) return;
-
-            console.log(channels);
-
-            res.render('timeline', {
-                global: {
-                    title: 'Startseite',
-                    loggedIn: req.session.loggedIn,
-                    page: 'timeline'
-                },
-                channels: channels
-            });
-        }
     }).on('error', function () {
         console.log('No Host');
     });
@@ -106,6 +122,117 @@ app.post('/login', function (req, res) {
     res.end();
 });
 
+app.get('/program', function (req, res) {
+    res.render('program', {
+        global: {
+            title: 'Program',
+            loggedIn: req.session.loggedIn,
+            page: 'program'
+        }
+    });
+});
 
+app.get('/logout', function (req, res) {
+    if (typeof(req.session.loggedIn) != 'undefined' && req.session.loggedIn) {
+        req.session.loggedIn = false;
+        res.redirect('/');
+        return;
+    } else {
+        res.redirect('/login');
+        return;
+    }
+});
+
+app.get('*', function(req, res) {
+    console.log('Requested unknown url: ' + req.url);
+    res.redirect('/timeline');
+});
 
 app.listen(8007);
+
+function ksort (inputArr, sort_flags) {
+    // Sort an array by key  
+    // 
+    // version: 1107.2516
+    // discuss at: http://phpjs.org/functions/ksort    // +   original by: GeekFG (http://geekfg.blogspot.com)
+    // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // +   improved by: Brett Zamir (http://brett-zamir.me)
+    // %          note 1: The examples are correct, this is a new way
+    // %        note 2: This function deviates from PHP in returning a copy of the array instead    // %        note 2: of acting by reference and returning true; this was necessary because
+    // %        note 2: IE does not allow deleting and re-adding of properties without caching
+    // %        note 2: of property position; you can set the ini of "phpjs.strictForIn" to true to
+    // %        note 2: get the PHP behavior, but use this only if you are in an environment
+    // %        note 2: such as Firefox extensions where for-in iteration order is fixed and true    // %        note 2: property deletion is supported. Note that we intend to implement the PHP
+    // %        note 2: behavior by default if IE ever does allow it; only gives shallow copy since
+    // %        note 2: is by reference in PHP anyways
+    // %        note 3: Since JS objects' keys are always strings, and (the
+    // %        note 3: default) SORT_REGULAR flag distinguishes by key type,    // %        note 3: if the content is a numeric string, we treat the
+    // %        note 3: "original type" as numeric.
+    // -    depends on: i18n_loc_get_default
+    // -    depends on: strnatcmp
+    // *     example 1: data = {d: 'lemon', a: 'orange', b: 'banana', c: 'apple'};    // *     example 1: data = ksort(data);
+    // *     results 1: {a: 'orange', b: 'banana', c: 'apple', d: 'lemon'}
+    // *     example 2: ini_set('phpjs.strictForIn', true);
+    // *     example 2: data = {2: 'van', 3: 'Zonneveld', 1: 'Kevin'};
+    // *     example 2: ksort(data);    // *     results 2: data == {1: 'Kevin', 2: 'van', 3: 'Zonneveld'}
+    // *     returns 2: true
+    var tmp_arr = {},
+        keys = [],
+        sorter, i, k, that = this,        strictForIn = false,
+        populateArr = {};
+ 
+    switch (sort_flags) {
+    case 'SORT_STRING':        // compare items as strings
+        sorter = function (a, b) {
+            return that.strnatcmp(a, b);
+        };
+        break;    case 'SORT_LOCALE_STRING':
+        // compare items as strings, based on the current locale (set with  i18n_loc_set_default() as of PHP6)
+        var loc = this.i18n_loc_get_default();
+        sorter = this.php_js.i18nLocales[loc].sorting;
+        break;    case 'SORT_NUMERIC':
+        // compare items numerically
+        sorter = function (a, b) {
+            return ((a + 0) - (b + 0));
+        };        break;
+        // case 'SORT_REGULAR': // compare items normally (don't change types)
+    default:
+        sorter = function (a, b) {
+            var aFloat = parseFloat(a),                bFloat = parseFloat(b),
+                aNumeric = aFloat + '' === a,
+                bNumeric = bFloat + '' === b;
+            if (aNumeric && bNumeric) {
+                return aFloat > bFloat ? 1 : aFloat < bFloat ? -1 : 0;            } else if (aNumeric && !bNumeric) {
+                return 1;
+            } else if (!aNumeric && bNumeric) {
+                return -1;
+            }            return a > b ? 1 : a < b ? -1 : 0;
+        };
+        break;
+    }
+     // Make a list of key names
+    for (k in inputArr) {
+        if (inputArr.hasOwnProperty(k)) {
+            keys.push(k);
+        }    }
+    keys.sort(sorter);
+ 
+    // BEGIN REDUNDANT
+    this.php_js = this.php_js || {};    this.php_js.ini = this.php_js.ini || {};
+    // END REDUNDANT
+    strictForIn = this.php_js.ini['phpjs.strictForIn'] && this.php_js.ini['phpjs.strictForIn'].local_value && this.php_js.ini['phpjs.strictForIn'].local_value !== 'off';
+    populateArr = strictForIn ? inputArr : populateArr;
+     // Rebuild array with sorted key names
+    for (i = 0; i < keys.length; i++) {
+        k = keys[i];
+        tmp_arr[k] = inputArr[k];
+        if (strictForIn) {            delete inputArr[k];
+        }
+    }
+    for (i in tmp_arr) {
+        if (tmp_arr.hasOwnProperty(i)) {            populateArr[i] = tmp_arr[i];
+        }
+    }
+ 
+    return strictForIn || populateArr;
+}
