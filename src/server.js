74d62a80546ca2d50364a716c4d65807a395697e
Kersten Burkhardt <kerstenk@gmail.com>
2011-09-04 01:54:52 +0200

    Added search logic, removed ksort and removed an unneeded if

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ var app = express.createServer();
 var rest = require('restler');
 var monomi = require("monomi");
 var config = require('./etc/config');
+var ksort = require('./lib/ksort');
 
 app.configure('development', function() {
 
@@ -140,11 +141,7 @@ app.post('/login', function (req, res) {
 });
 
 app.get('/program', function (req, res) {
-    var chan = 0;
-    
-    if (req.param("chan")) {
-        chan = req.param("chan")
-    }
+    var chan = req.param("chan", 0);
     
     rest.get(restfulUrl + '/channels/.json?&start=0').on('complete', function(data) {
         rest.get(restfulUrl + '/events/' + data.channels[chan].channel_id + '/86000.json?start=0').on('complete',  function (epg) {
@@ -170,20 +167,6 @@ app.get('/watch', function (req, res) {
             page: 'watch'
         }
     });
-});
-
-app.post('/program', function (req, res) {
-    var username = req.param("username");
-    var password = req.param("password");
-    
-    if (username == 'kersten' && password == 'manager') {
-        req.session.loggedIn = true;
-        res.redirect('/');
-    } else {
-        res.redirect('/login?failed=true');
-    }
-    
-    res.end();
 });
 
 app.get('/timer', function (req, res) {
@@ -214,13 +197,51 @@ app.get('/timer', function (req, res) {
 });
 
 app.get('/search', function (req, res) {
-    res.render('search', {
-        global: {
-            title: 'Search',
-            loggedIn: req.session.loggedIn,
-            page: 'search'
-        }
-    });
+    if (req.param("q", "") != "") {
+        var start = req.param("site", 1) * config.app.entries;
+    
+        rest.post(restfulUrl + '/events/search.json?start=' + start + '&limit=' + config.app.entries, {
+            data: {
+                query: req.param("q"),
+                mode: 0,
+                channelid: 0,
+                use_title: true,
+                use_subtitle: (req.param("s", false) != false) ? true : false,
+                use_description: (req.param("d", false) != false) ? true : false
+            }
+        }).on('complete', function(data) {
+            console.log(Math.floor(data.total / config.app.entries));
+            res.render('search', {
+                global: {
+                    title: 'Search',
+                    loggedIn: req.session.loggedIn,
+                    page: 'search'
+                },
+                results: data.events,
+                paginator: {
+                    total: data.total,
+                    cur: parseInt(req.param("site", 1)),
+                    sites: Math.floor(data.total / config.app.entries),
+                    next: parseInt(req.param("site", 1)) + 1,
+                    previous: parseInt(req.param("site", 1)) -1
+                },
+                q: req.param("q", ""),
+                s: req.param("s", ""),
+                d: req.param("d", "")
+            });
+        });
+    } else {
+        res.render('search', {
+            global: {
+                title: 'Search',
+                loggedIn: req.session.loggedIn,
+                page: 'search'
+            },
+            q: req.param("q", ""),
+            s: req.param("s", ""),
+            d: req.param("d", "")
+        });
+    }
 });
 
 app.get('/searchtimer', function (req, res) {
@@ -275,90 +296,3 @@ app.get('*', function(req, res) {
 });
 
 app.listen(config.web.port);
-
-function ksort (inputArr, sort_flags) {
-    // Sort an array by key  
-    // 
-    // version: 1107.2516
-    // discuss at: http://phpjs.org/functions/ksort    // +   original by: GeekFG (http://geekfg.blogspot.com)
-    // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-    // +   improved by: Brett Zamir (http://brett-zamir.me)
-    // %          note 1: The examples are correct, this is a new way
-    // %        note 2: This function deviates from PHP in returning a copy of the array instead    // %        note 2: of acting by reference and returning true; this was necessary because
-    // %        note 2: IE does not allow deleting and re-adding of properties without caching
-    // %        note 2: of property position; you can set the ini of "phpjs.strictForIn" to true to
-    // %        note 2: get the PHP behavior, but use this only if you are in an environment
-    // %        note 2: such as Firefox extensions where for-in iteration order is fixed and true    // %        note 2: property deletion is supported. Note that we intend to implement the PHP
-    // %        note 2: behavior by default if IE ever does allow it; only gives shallow copy since
-    // %        note 2: is by reference in PHP anyways
-    // %        note 3: Since JS objects' keys are always strings, and (the
-    // %        note 3: default) SORT_REGULAR flag distinguishes by key type,    // %        note 3: if the content is a numeric string, we treat the
-    // %        note 3: "original type" as numeric.
-    // -    depends on: i18n_loc_get_default
-    // -    depends on: strnatcmp
-    // *     example 1: data = {d: 'lemon', a: 'orange', b: 'banana', c: 'apple'};    // *     example 1: data = ksort(data);
-    // *     results 1: {a: 'orange', b: 'banana', c: 'apple', d: 'lemon'}
-    // *     example 2: ini_set('phpjs.strictForIn', true);
-    // *     example 2: data = {2: 'van', 3: 'Zonneveld', 1: 'Kevin'};
-    // *     example 2: ksort(data);    // *     results 2: data == {1: 'Kevin', 2: 'van', 3: 'Zonneveld'}
-    // *     returns 2: true
-    var tmp_arr = {},
-        keys = [],
-        sorter, i, k, that = this,        strictForIn = false,
-        populateArr = {};
- 
-    switch (sort_flags) {
-    case 'SORT_STRING':        // compare items as strings
-        sorter = function (a, b) {
-            return that.strnatcmp(a, b);
-        };
-        break;    case 'SORT_LOCALE_STRING':
-        // compare items as strings, based on the current locale (set with  i18n_loc_set_default() as of PHP6)
-        var loc = this.i18n_loc_get_default();
-        sorter = this.php_js.i18nLocales[loc].sorting;
-        break;    case 'SORT_NUMERIC':
-        // compare items numerically
-        sorter = function (a, b) {
-            return ((a + 0) - (b + 0));
-        };        break;
-        // case 'SORT_REGULAR': // compare items normally (don't change types)
-    default:
-        sorter = function (a, b) {
-            var aFloat = parseFloat(a),                bFloat = parseFloat(b),
-                aNumeric = aFloat + '' === a,
-                bNumeric = bFloat + '' === b;
-            if (aNumeric && bNumeric) {
-                return aFloat > bFloat ? 1 : aFloat < bFloat ? -1 : 0;            } else if (aNumeric && !bNumeric) {
-                return 1;
-            } else if (!aNumeric && bNumeric) {
-                return -1;
-            }            return a > b ? 1 : a < b ? -1 : 0;
-        };
-        break;
-    }
-     // Make a list of key names
-    for (k in inputArr) {
-        if (inputArr.hasOwnProperty(k)) {
-            keys.push(k);
-        }    }
-    keys.sort(sorter);
- 
-    // BEGIN REDUNDANT
-    this.php_js = this.php_js || {};    this.php_js.ini = this.php_js.ini || {};
-    // END REDUNDANT
-    strictForIn = this.php_js.ini['phpjs.strictForIn'] && this.php_js.ini['phpjs.strictForIn'].local_value && this.php_js.ini['phpjs.strictForIn'].local_value !== 'off';
-    populateArr = strictForIn ? inputArr : populateArr;
-     // Rebuild array with sorted key names
-    for (i = 0; i < keys.length; i++) {
-        k = keys[i];
-        tmp_arr[k] = inputArr[k];
-        if (strictForIn) {            delete inputArr[k];
-        }
-    }
-    for (i in tmp_arr) {
-        if (tmp_arr.hasOwnProperty(i)) {            populateArr[i] = tmp_arr[i];
-        }
-    }
- 
-    return strictForIn || populateArr;
-}
