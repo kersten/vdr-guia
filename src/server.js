239478ba291d6131dc2f596116fa2ec1bbab8970
Kersten Burkhardt <kerstenk@gmail.com>
2011-09-23 10:42:13 +0200

    Switched to SSL

--- src/server.js
+++ src/server.js
@@ -1,11 +1,17 @@
 var express = require('express');
-var app = express.createServer();
-var io = require('socket.io').listen(app);
+var fs = require("fs");
+var crypto = require('crypto');
 var config = require('./etc/config');
+
+var privateKey = fs.readFileSync(__dirname + '/etc/cert/server.key').toString();
+var certificate = fs.readFileSync(__dirname + '/etc/cert/server.crt').toString();
+var credentials = crypto.createCredentials({key: privateKey, cert: certificate});
+
+var app = express.createServer({key: privateKey, cert: certificate});
+var io = require('socket.io').listen(app);
 
 require('./mvc').boot(app, io);
 
 app.listen(config.web.port);
+
 console.log('Express app started on port ' + config.web.port);
-
-
