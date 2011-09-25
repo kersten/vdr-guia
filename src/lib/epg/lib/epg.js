80f0f1d610ab8697e8eb62d00433d10c95aa2293
Kersten Burkhardt <kerstenk@gmail.com>
2011-09-25 23:50:05 +0200

    Added epg lib

---
+++ src/lib/epg/lib/epg.js
@@ -0,0 +1,35 @@
+var entry = function () {
+    
+};
+
+entry.prototype.hasActor = function (name) {
+    
+};
+
+entry.prototype.hasCountry = function (name) {
+    
+};
+
+entry.prototype.hasRegisseur = function (name) {
+    
+};
+
+entry.prototype.hasYear = function (name) {
+    
+};
+
+entry.prototype.getActors = function () {
+    
+};
+
+entry.prototype.getCountry = function () {
+    
+};
+
+entry.prototype.getRegisseur = function () {
+    
+};
+
+entry.prototype.getYear = function () {
+    
+};
