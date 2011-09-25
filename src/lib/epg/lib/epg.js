daacce7fce6b7066adce7192746e71a5a3830dd4
Kersten Burkhardt <kerstenk@gmail.com>
2011-09-25 23:54:45 +0200

    some data to epg lib

--- src/lib/epg/lib/epg.js
+++ src/lib/epg/lib/epg.js
@@ -1,5 +1,23 @@
-var entry = function () {
-    
+var entry = function (entry) {
+    this.data = {
+        title: null,
+        short_description: null,
+        description: null,
+        gerne: null,
+        rating: null,
+        actors: null,
+        country: null,
+        regisseur: null,
+        year: null,
+        sequence: null,
+        start_time: 1316983500,
+        channel: 'S19.2E-1-1011-11120',
+        duration: 4200,
+        images: 1,
+        timer_exists: false,
+        timer_active: false,
+        timer_id: ''
+    };
 };
 
 entry.prototype.hasActor = function (name) {
