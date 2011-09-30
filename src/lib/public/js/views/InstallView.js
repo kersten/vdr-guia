89f7ef767092fad58faf389d55cde5e04efe8cd4
Kersten Burkhardt <kerstenk@gmail.com>
2011-09-30 23:42:57 +0200

    First install steps

--- src/lib/public/js/views/InstallView.js
+++ src/lib/public/js/views/InstallView.js
@@ -1,5 +1,28 @@
 var InstallView = Backbone.View.extend({
+    
     initialize: function () {
-        console.log('Installation loaded ..');
+        $(document).attr('title', 'GUIA // Installation');
+        this.render();
+    },
+    
+    events: {
+        'click #next': 'loadNextSite',
+        'click #previous': 'loadNextSite'
+    },
+    
+    loadNextSite: function (event) {
+        var nextSite = $(event.currentTarget).attr('next') + 'Template';
+        var template = _.template( $("#" + nextSite).html(), {} );
+        
+        this.el.html( template );
+        return this;
+    },
+    
+    render: function () {
+        // Compile the template using underscore
+        var template = _.template( $("#InstallViewTemplate").html(), {} );
+        // Load the compiled HTML into the Backbone "el"
+        this.el.html( template );
+        return this;
     }
 });
