a326e5844f4801ed5926d9ce80039f87cb4d8e4f
Volker Richert <richert@addmore.de>
2011-11-17 21:50:42 +0100

    start & stop working in other browser than Chrome

--- src/lib/public/js/collections/TimerCollection.js
+++ src/lib/public/js/collections/TimerCollection.js
@@ -4,8 +4,22 @@ var TimerCollection = Backbone.Collection.extend({
     
     parse: function (response) {
         response.forEach(function (item, index) {
-            var start = new Date(item.start_timestamp);
-            var stop = new Date(item.stop_timestamp);
+            var start = new Date(
+                item.start_timestamp.substr(0,4),
+                item.start_timestamp.substr(5,2),
+                item.start_timestamp.substr(8,2),
+                item.start_timestamp.substr(11,2),
+                item.start_timestamp.substr(14,2),
+                item.start_timestamp.substr(17,2)
+            );
+            var stop = new Date(
+                item.stop_timestamp.substr(0,4),
+                item.stop_timestamp.substr(5,2),
+                item.stop_timestamp.substr(8,2),
+                item.stop_timestamp.substr(11,2),
+                item.stop_timestamp.substr(14,2),
+                item.stop_timestamp.substr(17,2)
+            );
 
             item.start = ((start.getHours() < 10) ? '0' : '') + start.getHours() + ':' + ((start.getMinutes() < 10) ? '0' : '') + start.getMinutes();
             item.stop = ((stop.getHours() < 10) ? '0' : '') + stop.getHours() + ':' + ((stop.getMinutes() < 10) ? '0' : '') + stop.getMinutes();
