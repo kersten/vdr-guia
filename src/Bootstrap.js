bf7e22bd9bca03774a95771ac1fcebdca474952e
Kersten Burkhardt <kerstenk@gmail.com>
2011-11-30 00:16:12 +0100

    Moved views out of bootstrap.

--- src/Bootstrap.js
+++ src/Bootstrap.js
@@ -234,80 +234,17 @@ Bootstrap.prototype.setupViews = function () {
         });
     });
     
-    this.app.get('/templates/*', function (req, res) {
-        if (req.session.loggedIn) {
-            var template = req.url.substr(1);
-            res.render(template, {
-                layout: false
-            });
-        } else {
-            if (installed) {
-                if (req.url != '/templates/contact') {
-                    req.url = '/templates/welcome';
-                }
-            }
-            
-            res.render(req.url.substr(1), {
-                layout: false
-            });
-        }
-    });
+    var self = this;
     
-    var LogoSchema = require('./schemas/LogoSchema');
-    
-    this.app.get('/logo/*', function (req, res) {
-        if (req.session.loggedIn) {
-            var channel_name = unescape(req.url.substr(6)).replace(/\//, '|');
+    fs.readdir(__dirname + '/views', function (err, files) {
+        if (err) throw err;
+        files.forEach(function (file) {
+            file = file.replace('.js', '');
             
-            LogoSchema.findOne({name: channel_name}, function (err, data) {
-                if (err || data == null) {
-                    console.log('Logo ' + channel_name + ' not found, getting placeholder');
-                    
-                    var http_client = http.createClient(80, 'placehold.it');
-                    var image_get_request = http_client.request('GET', 'http://placehold.it/90x51.png&text=' + req.url.substr(6), {
-                        'Host': 'placehold.it',
-                        "User-Agent": 'Firefox/7.0.1',
-                        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
-                    });
-                    
-                    image_get_request.addListener('response', function(proxy_response){
-                        var current_byte_index = 0;
-                        var response_content_length = parseInt(proxy_response.header("Content-Length"));
-                        var response_body = new Buffer(response_content_length);
-                        
-                        proxy_response.setEncoding('binary');
-                        proxy_response.addListener('data', function(chunk){
-                            response_body.write(chunk, current_byte_index, "binary");
-                            current_byte_index += chunk.length;
-                        });
-                        proxy_response.addListener('end', function(){
-                            fs.writeFile(__dirname + '/share/logos/' + channel_name + '.png', response_body, function (err) {
-                                var logoModel = new LogoSchema({
-                                    file: channel_name + '.png',
-                                    name: channel_name
-                                });
-                                
-                                logoModel.save();
-                            });
-                            
-                            res.contentType('image/png');
-                            res.send(response_body);
-                        });
-                    });
-                    
-                    image_get_request.end();
-                } else {
-                    var filename = __dirname + '/share/logos/' + data.file;
-                    res.contentType(filename);
-                    
-                    var image = fs.readFileSync(filename);
-                    
-                    res.end(image);
-                }
-            });
-        } else {
-            res.status(403);
-        }
+            var view = require(__dirname + '/views/' + file);
+            
+            self.app.get(view.url, view.func);
+        });
     });
 };
 
---
+++ src/views/EventImageView.js
@@ -0,0 +1,8 @@
+var EventImageView = {
+    url: '/events/image/:eventid/:imageid',
+    func: function (req, res) {
+        // vdr.restful +'/events/iamge/' + req.params.eventid + '/' +req.params.imageid
+    }
+};
+
+module.exports = EventImageView;
