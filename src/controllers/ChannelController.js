c079117808a8a0021adb2c64600a53d165e96dc5
Kersten Burkhardt <kerstenk@gmail.com>
2012-01-11 21:35:54 +0100

    fixed

--- src/controllers/ChannelController.js
+++ src/controllers/ChannelController.js
@@ -1,14 +1,16 @@
 var Channel = require('../lib/Channel');
 
 io.sockets.on('connection', function (socket) {
-    if (socket.handshake.session.loggedIn) {
-        socket.on('ChannelCollection:read', function (data, callback) {
-            var channels = new Channel();
-            var options = data;
+    socket.on('ChannelCollection:read', function (data, callback) {
+        if (socket.handshake.session.loggedIn) {
+            return false;
+        }
+        
+        var channels = new Channel();
+        var options = data;
 
-            channels.getAll(options, function (channels) {
-                callback(channels);
-            });
+        channels.getAll(options, function (channels) {
+            callback(channels);
         });
-    }
+    });
 });
