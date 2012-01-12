600fd20f8d9d32d9a0e8d6badcf2445f4cdb46b1
Kersten Burkhardt <kerstenk@gmail.com>
2012-01-12 20:51:36 +0100

    Activate all channels at the same time

--- src/share/www/js/views/Settings/ChannelsView.js
+++ src/share/www/js/views/Settings/ChannelsView.js
@@ -1,44 +1,38 @@
 var SettingsChannelsView = Backbone.View.extend({
     template: 'SettingsChannelsTemplate',
     className: 'span14 columns',
-    
+
+    events: {
+        'change #selectAllChannels': 'changeAllStates'
+    },
+
     initialize: function () {
         $(this.el).html(_.template( $('#' + this.template).html(), {} ));
-        
+
         this.channellist = new ChannelCollection();
 
         var self = this;
         this.channellist.fetch({
             success: function (collection) {
                 collection.forEach(function (channel) {
-                    var row = $('<tr></tr>').attr('_id', channel.get('_id'));
-                    row.append($('<td></td>').html(channel.get('number')));
-
-                    var active = $('<input></input>').attr('type', 'checkbox');
-
-                    active.bind('change', function () {
-                        if ($(this).is(':checked')) {
-                            channel.set({active: true});
-                        } else {
-                            channel.set({active: false});
-                        }
-
-                        channel.save(function (doc) {
-                            console.log(doc);
-                        });
+                    var channelView = new SettingsChannelsChannelView({
+                        model: channel
                     });
 
-                    if (channel.get('active') == true) {
-                        active.attr('checked', 'checked');
-                    }
-
-                    row.append($('<td></td>').html(active));
-                    row.append($('<td></td>').html(channel.get('name')));
-
-                    $('#channels > tbody', self.el).append(row);
+                    $('#channels > tbody', self.el).append(channelView.render().el);
                 });
             }
         });
+    },
+
+    changeAllStates: function (ev) {
+        if ($(ev.currentTarget).is(':checked')) {
+            $('input.channel', this.el).attr('checked', true);
+        } else {
+            $('input.channel', this.el).attr('checked', false);
+        }
+
+        $('input.channel', this.el).trigger('change')
     },
 
     render: function () {
