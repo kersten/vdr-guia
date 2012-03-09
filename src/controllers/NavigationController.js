176f539f68d0106abdb59149aaeef7e42205e57b
Kersten Burkhardt <kerstenk@gmail.com>
2012-03-09 14:47:17 +0100

    Dropdown for prgram navigation

--- src/controllers/NavigationController.js
+++ src/controllers/NavigationController.js
@@ -1,50 +1,74 @@
+var Channel = require('../lib/Channel');
+
 io.sockets.on('connection', function (socket) {
     socket.on('NavigationCollection:read', function (data, callback) {
         if (socket.handshake.session.loggedIn) {
-            var menu = [{
-                title: __('Highlights'),
-                icon: 'heart',
-                view: 'Highlights'
-            }, {
-                title: __('TV Guide'),
-                icon: 'list',
-                view: 'TVGuide'
-            }, {
-                title: __('Program'),
-                icon: 'book',
-                view: 'Program'
-            }, {
-                title: __('Recordings'),
-                icon: 'facetime-video',
-                view: 'Recordings'
-            }, {
-                title: __('Me'),
-                icon: 'user',
-                items: [{
-                    title: __('My Profile'),
-                    icon: 'user',
-                    view: 'Me'
-                }, {
-                    title: __('Help'),
-                    icon: 'question-sign',
-                    view: 'Help'
-                }, {
-                    title: __('Shortcuts'),
-                    icon: 'cog',
-                    view: 'Shortcuts'
-                }, {
-                    title: __('Settings'),
-                    icon: 'edit',
-                    view: 'Settings'
-                }, {
-                    title: __('Logout'),
-                    icon: 'off',
-                    link: 'logout',
-                    id: 'logoutBtn'
-                }]
-            }];
+            var channels = new Channel();
+            channels.getAll({active: true}, function (docs) {
+                var channels_array = [];
 
-            callback({items: menu, loggedIn: true});
+                if (docs) {
+                    docs.forEach(function (channel) {
+                        channels_array.push({
+                            title: channel.get('name'),
+                            icon: 'user',
+                            view: 'Program/' + channel.get('_id')
+                        });
+                    });
+                }
+
+                var program = {
+                    title: __('Program'),
+                    icon: 'book'
+                };
+
+                if (channels_array.length == 0) {
+                    program.view = 'Program';
+                } else {
+                    program.items = channels_array;
+                }
+
+                var menu = [{
+                    title: __('Highlights'),
+                    icon: 'heart',
+                    view: 'Highlights'
+                }, {
+                    title: __('TV Guide'),
+                    icon: 'list',
+                    view: 'TVGuide'
+                }, program, {
+                    title: __('Recordings'),
+                    icon: 'facetime-video',
+                    view: 'Recordings'
+                }, {
+                    title: __('Me'),
+                    icon: 'user',
+                    items: [{
+                        title: __('My Profile'),
+                        icon: 'user',
+                        view: 'Me'
+                    }, {
+                        title: __('Help'),
+                        icon: 'question-sign',
+                        view: 'Help'
+                    }, {
+                        title: __('Shortcuts'),
+                        icon: 'cog',
+                        view: 'Shortcuts'
+                    }, {
+                        title: __('Settings'),
+                        icon: 'edit',
+                        view: 'Settings'
+                    }, {
+                        title: __('Logout'),
+                        icon: 'off',
+                        link: 'logout',
+                        id: 'logoutBtn'
+                    }]
+                }];
+
+                callback({items: menu, loggedIn: true});
+            });
         } else {
             var menu = [{
                 title: __('Home'),
