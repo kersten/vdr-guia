7251fe6ffb6e4e3a5d6474f10b7a94f41a370183
Kersten Burkhardt <kerstenk@gmail.com>
2012-03-23 00:20:27 +0100

    Cleaned Plugins

--- src/application/frontend/plugins/yaVDR/public/js/views/Yavdr/CommandsView.js
+++
@@ -1,8 +0,0 @@
-var YavdrCommandsView = Backbone.View.extend({
-    template: "yaVDRCommandsTemplate",
-
-    render: function () {
-        $(this.el).html(_.template( $('#' + this.template).html(), {} ));
-        return this;
-    }
-});
