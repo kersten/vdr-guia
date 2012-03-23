efcd403267ad6cc64440985229c12395fd92d06c
Kersten Burkhardt <kerstenk@gmail.com>
2012-03-23 22:19:51 +0100

    some test stuff

--- src/application/frontend/plugins/Base/public/js/views/WelcomeView.js
+++ src/application/frontend/plugins/Base/public/js/views/WelcomeView.js
@@ -1,8 +1,52 @@
 var WelcomeView = Backbone.View.extend({
     template: 'BaseWelcomeTemplate',
+    
+    animate: function () {
+    	var _this = this;
+    
+    	requestAnimationFrame( function () {
+    		_this.animate();
+    	} );
+    	
+        this.renderGL();
+    },
+    
+    renderGL: function () {
+    	this.mesh.rotation.x += 0.01;
+        this.mesh.rotation.y += 0.02;
+
+        this.renderer.render( this.scene, this.camera );
+    },
 
     render: function () {
         $(this.el).html(_.template( $('#' + this.template).html(), {} ));
+
+		var camera, scene, renderer,
+    		geometry, material, mesh;
+
+    	
+    	this.scene = new THREE.Scene();
+
+        this.camera = new THREE.PerspectiveCamera( 75, 940 / window.innerHeight, 1, 10000 );
+        this.camera.position.z = 1000;
+        this.scene.add( this.camera );
+
+        geometry = new THREE.CubeGeometry( 200, 200, 200 );
+        material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: false } );
+
+        this.mesh = new THREE.Mesh( geometry, material );
+        this.scene.add( this.mesh );
+
+        this.renderer = new THREE.WebGLRenderer();
+        this.renderer.setSize( 940, window.innerHeight );
+        
+        $(this.el).html(this.renderer.domElement);
+    	
+    	this.animate.apply(this, []);
+		
+		
+		
+		return this;
 
         var cloudCollection = new EventCollection();
         cloudCollection.url = 'EventCollection:cloud';
