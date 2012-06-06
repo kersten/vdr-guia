ff3075e88bcce76d8197b014acace58511d76b0e
Kersten Burkhardt <kerstenk@gmail.com>
2012-06-06 19:20:46 +0200

    New test

--- test.js
+++ test.js
@@ -1,4 +1,34 @@
-var natural = require('natural'),
+var sparql = require('sparql');
+
+var client = new sparql.Client('http://dbpedia.org/sparql');
+
+client.query('PREFIX owl: <http://www.w3.org/2002/07/owl#> \
+        PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> \
+        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> \
+        PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> \
+        PREFIX foaf: <http://xmlns.com/foaf/0.1/> \
+        PREFIX dc: <http://purl.org/dc/elements/1.1/> \
+        PREFIX : <http://dbpedia.org/resource/> \
+        PREFIX dbpedia2: <http://dbpedia.org/property/> \
+        PREFIX dbpedia: <http://dbpedia.org/> \
+        PREFIX skos: <http://www.w3.org/2004/02/skos/core#> \
+        PREFIX dbo: <http://dbpedia.org/ontology/> \
+    SELECT * { \
+        :RTL_Television ?p ?f. \
+        ?f rdfs:label ?l \
+    }', function (err, res) {
+    if (err) {
+        console.log(err);
+    }
+
+    res.results.bindings.forEach(function (res) {
+        //console.log(res.l['xml:lang']);
+        console.log(res);
+    });
+});
+
+
+/*var natural = require('natural'),
     TfIdf = natural.TfIdf,
     tfidf = new TfIdf();
     
@@ -15,7 +45,7 @@ tfidf.tfidfs('das', function(i, measure) {
 console.log('Kokowääh --------------------------------');
 tfidf.tfidfs('Kokowääh', function(i, measure) {
     console.log('document #' + i + ' is ' + measure);
-});
+});*/
 
 // [ 'your', 'dog', 'has', 'flees' ]
 // http://192.168.0.5:8008/recstream.html?recid=recording_e12420a2e7faf6bb3f1e6bb0d2cb2371
